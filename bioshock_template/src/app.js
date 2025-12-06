f5326b1b428c385dad201c4a88afa2b23c049a45
Tyler Conwell <wgc14@txstate.edu>
2025-12-06 17:30:34 -0600

    added navbar to all the pages in routes

--- bioshock_template/src/app.js
+++ bioshock_template/src/app.js
@@ -1,11 +1,14 @@
 import "./Bioshock_ecomm_new/bioshock_template/src/App.css"
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 // imports needed for react router dom to work, first time using it
+import {Navbar} from "./Bioshock_ecomm_new/bioshock_template/src/components/navbar.jsx"
 
 function App() {
     return <div className="App">
 
         <Router>
+            {/* adding the navbar above to its in all pages */}
+            <Navbar />
             <Routes>
                 {/* main page or shop of the products */}
                 <Route path="/" />
