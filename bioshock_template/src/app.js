99a89ca81c757fc3c40e21b0019bf6badac09132
Tyler Conwell <wgc14@txstate.edu>
2025-12-06 15:55:08 -0600

    adding the dependencies and router dom

---
+++ bioshock_template/src/app.js
@@ -0,0 +1,9 @@
+import "./Bioshock_ecomm_new/bioshock_template/src/App.css"
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+// imports needed for react router dom to work, first time using it
+
+function App() {
+    return <div className="App">test</div>;
+}
+
+export default App;
