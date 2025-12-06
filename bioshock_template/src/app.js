e1febbde77b7fab7e6c2e67a92c5360b68ea9b85
Tyler Conwell <wgc14@txstate.edu>
2025-12-06 16:57:01 -0600

    adding files, navbar, component & pages

--- bioshock_template/src/app.js
+++ bioshock_template/src/app.js
@@ -3,7 +3,17 @@ import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 // imports needed for react router dom to work, first time using it
 
 function App() {
-    return <div className="App">test</div>;
+    return <div className="App">
+
+        <Router>
+            <Routes>
+                {/* main page or shop of the products */}
+                <Route path="/" />
+                {/* cart page path */}
+                <Route path="/cart" />
+            </Routes>
+        </Router>
+        </div>;
 }
 
 export default App;
