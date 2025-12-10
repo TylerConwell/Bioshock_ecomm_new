f75d1740048f17abc74dc9ddd81a0bee252ab8d6
Tyler Conwell <wgc14@txstate.edu>
2025-12-10 11:37:45 -0600

    adding css to the navbar

--- bioshock_template/src/components/navbar.jsx
+++ bioshock_template/src/components/navbar.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import {Link} from 'react-router-dom'
+import {ShoppingCart} from "phosphor-react";
+import  "./navbar.css"
 
 export const Navbar = () => {
     return (
         <div className="navbar">
             <div className="links">
                 <Link to="/"> Shop</Link>
-                <Link to="/cart">cart</Link>
+                <Link to="/cart">
+                    <ShoppingCart size={32} />
+                </Link>
+
                 <Link to="/Vigors_test">         Vigor Test</Link>
                 <Link to="/plasmid_test">   Plasmid Test</Link>
             </div>
